8febc44f7e625ddf1336857a92a0a6198aedd76e
LarceRR <larcerrtc@gmail.com>
2026-08-11 08:08:09 +0300

    fix(m1): make channel access errors HTTP-safe and return updated settings version

--- apps/api/src/modules/channels/channels.module.ts
+++ apps/api/src/modules/channels/channels.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
 import { SystemModule } from '../system/system.module';
-import { AuditLogService } from '../system/application/audit-log.service';
 import { ChannelsController } from './presentation/channels.controller';
 import { ChannelsService } from './application/channels.service';
 
 @Module({
   imports: [SystemModule],
   controllers: [ChannelsController],
-  providers: [ChannelsService, AuditLogService],
+  providers: [ChannelsService],
   exports: [ChannelsService],
 })
 export class ChannelsModule {}
